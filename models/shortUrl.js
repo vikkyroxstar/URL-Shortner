969f33c7af8973675989443b150ad7e6c610d0c4
vikkyroxstar9 <vivekroxstar9@gmail.com>
2020-08-10 13:20:15 +0530

    Url Shortner

---
+++ models/shortUrl.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose');
+const shortId = require('shortid');
+const shortUrlSchema = new mongoose.Schema({
+    full:{
+        type:String,
+        require:true
+    },
+    short:{
+        type:String,
+        required:true,
+        default:shortId.generate
+    },
+    clicks:{
+        type:Number,
+        require:true,
+        default:0
+    }
+});
+
+module.exports=mongoose.model('shortUrls',shortUrlSchema);
